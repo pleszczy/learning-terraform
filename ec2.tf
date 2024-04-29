5ec4845deb870f12ea9795b814f3a440f4068a4b
Piotr Leszczynski <piotrkun@gmail.com>
2024-04-29 20:11:32 +0200

    Fetching latest ubuntu ami from a data source

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 
 resource "aws_instance" "testInstance" {
-  ami                         = var.ami
+  ami                         = data.aws_ami.ubuntu.id
   instance_type               = var.instance_type
   availability_zone           = var.azs.1
   subnet_id                   = aws_subnet.public.1.id
@@ -9,7 +9,7 @@ resource "aws_instance" "testInstance" {
 
 resource "aws_instance" "testInstanceX4" {
   count         = 4
-  ami           = var.ami
+  ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
 
   tags = {
