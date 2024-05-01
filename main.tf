32a28d13be26ccc0b8f1a27606306dc46e54960e
Piotr Leszczynski <piotrkun@gmail.com>
2024-05-01 16:47:24 +0200

    Setting up terraform state in s3

--- main.tf
+++ main.tf
@@ -7,11 +7,18 @@ terraform {
       version = "~> 4.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "piotr-leszczynski-big-data-solutions"
+    key            = "state/terraform.tfstate"
+    region         = "eu-central-1"
+    encrypt        = true
+  }
+
 }
 
 provider "aws" {
   region     = var.region
   access_key = var.accessKey
-  secret_key = var.secretKey
-}
+  secret_key = var.secretKey}
 
--- s3.tf
+++ s3.tf
@@ -7,20 +7,3 @@ resource "aws_s3_object" "terraform_backend_folder" {
   key     = "state/"
   content = "Terraform backend"
 }
-
-# resource "aws_s3_bucket_policy" "storage_policy" {
-#   bucket = aws_s3_bucket.root.id
-#   policy = data.aws_iam_policy_document.storage_policy_doc.json
-# }
-
-# data "aws_iam_policy_document" "storage_policy_doc" {
-#   statement {
-#     actions   = ["s3:ListBucket"]
-#     resources = ["arn:aws:s3:::piotr-leszczynski-big-data-solutions"]
-#     effect    = "Allow"
-#     principals {
-#       type        = "AWS"
-#       identifiers = ["544385058064"]
-#     }
-#   }
-# }
